a653636ce04b390ec438c3ef6c4e39d91c572627
Everton Freitas <evertonfreitas59@hotmail.com>
2026-06-02 01:50:44 +0000

    gif bluey e frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home () {
-    return <h1>Olá mundo</h1>
+    return <>te amo filha <img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExOHp0cmM1a2twbjBmcXk5cm82NjhndGJqcHhkbXJyYnQ2MzR3MHp0cSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/j6TmWtNerkGPgrpxNF/giphy.gif" /></>
+
 }
 
 export default Home
